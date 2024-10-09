e3eecf9ff22c54fab007ca6ac144784cf7816168
lizhirong <zhirong.li@spacemit.com>
2024-10-09 15:22:13 +0800

    sc031 sensor draw image by mode 0 or 1 in 84fps

--- sensors/module/sc031gs_spm/sc031_spm.c
+++ sensors/module/sc031gs_spm/sc031_spm.c
@@ -9,7 +9,8 @@
 #include "sc031_spm.h"
 #include "sc031_spm_setting.h"
 
-static const uint8_t module_i2c_addr = 0x30; /* I2C Address 7-bit*/
+//static const uint8_t module_i2c_addr = 0x30; /* I2C Address 7-bit*/
+static const uint8_t module_i2c_addr = 0x32; /* I2C Address 7-bit*/
 
 struct regval_tab sc031_spm_vendor_id[] = {
     {0x3108, 0x31},
@@ -61,7 +62,7 @@ static int sc031_spm_get_sensor_cap_size(int32_t* capArraySize)
 {
     SENSORS_CHECK_PARA_POINTER(capArraySize);
 
-    *capArraySize = 1;
+    *capArraySize = SC031_SPM_WORK_MODE_SIZE;
     return 0;
 }
 
@@ -81,7 +82,7 @@ static int sc031_spm_get_sensor_capbility(int32_t capArraySize, SENSOR_CAPABILIT
     sensor_capability->snr_config_num = SC031_SPM_WORK_MODE_SIZE;
     for (i = 0; i < SC031_SPM_WORK_MODE_SIZE; i++) {
         switch (i) {
-            case SC031_SPM_640x480_10bit_LINEAR_30_4LANE: {
+            case SC031_SPM_640x480_10bit_LINEAR_30_1LANE: {
                 sensor_capability->snr_config[i].width = 640;
                 sensor_capability->snr_config[i].height = 480;
                 sensor_capability->snr_config[i].bitDepth = 10;
@@ -89,9 +90,22 @@ static int sc031_spm_get_sensor_capbility(int32_t capArraySize, SENSOR_CAPABILIT
                 sensor_capability->snr_config[i].minFps = 25;
                 sensor_capability->snr_config[i].image_mode = SENSOR_LINEAR_MODE;
                 sensor_capability->snr_config[i].lane_num = 1;
-                sensor_capability->snr_config[i].pattern = ISP_BAYER_PATTERN_BGGR;
+                sensor_capability->snr_config[i].pattern = ISP_BAYER_PATTERN_MONO;//ISP_BAYER_PATTERN_BGGR;
                 sensor_capability->snr_config[i].supportPDAF = 0;
-                sensor_capability->snr_config[i].work_mode = SC031_SPM_640x480_10bit_LINEAR_30_4LANE;
+                sensor_capability->snr_config[i].work_mode = SC031_SPM_640x480_10bit_LINEAR_30_1LANE;
+                sensor_capability->snr_config[i].setting = &sc031_spm_setting;
+            } break;
+            case SC031_SPM_640x480_10bit_LINEAR_50_1LANE: {
+                sensor_capability->snr_config[i].width = 640;
+                sensor_capability->snr_config[i].height = 480;
+                sensor_capability->snr_config[i].bitDepth = 10;
+                sensor_capability->snr_config[i].maxFps = 50;
+                sensor_capability->snr_config[i].minFps = 45;
+                sensor_capability->snr_config[i].image_mode = SENSOR_LINEAR_MODE;
+                sensor_capability->snr_config[i].lane_num = 1;
+                sensor_capability->snr_config[i].pattern = ISP_BAYER_PATTERN_MONO;//ISP_BAYER_PATTERN_BGGR;
+                sensor_capability->snr_config[i].supportPDAF = 0;
+                sensor_capability->snr_config[i].work_mode = SC031_SPM_640x480_10bit_LINEAR_50_1LANE;
                 sensor_capability->snr_config[i].setting = &sc031_spm_setting;
             } break;
             default: {
@@ -112,7 +126,7 @@ static int sc031_spm_get_sensor_work_info(int32_t work_mode, SENSOR_WORK_INFO_S*
     // snr_info->id_table_size = ARRAY_SIZE(sc031_spm_vendor_id);
 
     switch (work_mode) {
-        case SC031_SPM_640x480_10bit_LINEAR_30_4LANE: {
+        case SC031_SPM_640x480_10bit_LINEAR_30_1LANE: {
             snr_info->linetime = 12194;  //ns, Tline = hts/pclk
             snr_info->vts = 2732;
             snr_info->f32maxFps = 30;
@@ -122,6 +136,18 @@ static int sc031_spm_get_sensor_work_info(int32_t work_mode, SENSOR_WORK_INFO_S*
             snr_info->image_mode = SENSOR_LINEAR_MODE;
             snr_info->setting_table = sc031_spm_640x480_10bit_30fps_tab;
             snr_info->setting_table_size = ARRAY_SIZE(sc031_spm_640x480_10bit_30fps_tab);
+            snr_info->mipi_clock = 720;  // Mhz
+        } break;
+        case SC031_SPM_640x480_10bit_LINEAR_50_1LANE: {
+            snr_info->linetime = 12202;  //ns, Tline = hts/pclk
+            snr_info->vts = 1639;
+            snr_info->f32maxFps = 50;
+            snr_info->exp_time[0] = 1639 * snr_info->linetime / 1000;
+            snr_info->again[0] = 1 * 0x100;   // Q8 format
+            snr_info->dgain[0] = 1 * 0x1000;  // Q12 format
+            snr_info->image_mode = SENSOR_LINEAR_MODE;
+            snr_info->setting_table = sc031_spm_640x480_10bit_50fps_tab;
+            snr_info->setting_table_size = ARRAY_SIZE(sc031_spm_640x480_10bit_50fps_tab);
             snr_info->mipi_clock = 720;  // Mhz
         } break;
         default: {
