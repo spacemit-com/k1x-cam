fe3970a4a7ef9e256726e48bd22aa778c51e830d
xuhaodong <haodong.xu@spacemit.com>
2025-11-28 11:29:39 +0800

    support sc533hai for 2688*1616@30fps and 1080p@60fps(crop)

--- sensors/cam_sensors_module_list.h
+++ sensors/cam_sensors_module_list.h
@@ -32,6 +32,7 @@ extern SENSOR_OBJ_S ov16a10Obj;
 extern SENSOR_OBJ_S sc031Obj;
 extern SENSOR_OBJ_S sc501aiObj;
 extern SENSOR_OBJ_S sc520csObj;
+extern SENSOR_OBJ_S sc533haiObj;
 extern SENSOR_OBJ_S ov8856Obj;
 extern SENSOR_OBJ_S og02b10Obj;
 extern SENSOR_OBJ_S mlx75027Obj;
@@ -60,6 +61,7 @@ extern MODULE_OBJ_S ov16a10_spm_Obj;
 extern MODULE_OBJ_S sc031_spm_Obj;
 extern MODULE_OBJ_S sc501ai_spm_Obj;
 extern MODULE_OBJ_S sc520cs_spm_Obj;
+extern MODULE_OBJ_S sc533hai_spm_Obj;
 extern MODULE_OBJ_S ov8856_spm_Obj;
 extern MODULE_OBJ_S og02b10_spm_Obj;
 extern MODULE_OBJ_S mlx75027_spm_Obj;
@@ -102,6 +104,7 @@ SENSORS_MODULE_OBJ_S sensors_module_list[] = {
     {&sc031_spm_Obj, &sc031Obj, NULL, NULL},
     {&sc501ai_spm_Obj, &sc501aiObj, NULL, NULL},
     {&sc520cs_spm_Obj, &sc520csObj, NULL, NULL},
+    {&sc533hai_spm_Obj, &sc533haiObj, NULL, NULL},
     {&ov8856_spm_Obj, &ov8856Obj, NULL, NULL},
     {&og02b10_spm_Obj, &og02b10Obj, NULL, NULL},
     {&mlx75027_spm_Obj, &mlx75027Obj, NULL, NULL},
